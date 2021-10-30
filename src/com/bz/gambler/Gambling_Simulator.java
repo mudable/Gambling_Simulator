11527138e612393d4bed23d4f70bc4bf382bb858
mudable <mudableshilpa@gmail.com>
2021-10-30 14:51:15 +0530

    Added : UC3 to calculate win stake and loose stake

--- src/com/bz/gambler/Gambling_Simulator.java
+++ src/com/bz/gambler/Gambling_Simulator.java
@@ -2,18 +2,30 @@ package com.bz.gambler;
 
 public class Gambling_Simulator {
 	/*
-	 * Initialized variable with given values, as start stake and bet.
-	 * Added random method to check win or loose.
+	 * Initialized variable with given values, as start stake and bet. Added random
+	 * method to check win or loose.
 	 */
 	int start_stake = 100;
 	int bet = 1;
 
 	public void makeBet() {
-		int result = (int) (Math.random() * 10) % 2;
-		if (result == 0) {
-			start_stake -= 1;
-		} else
-			start_stake += 1;
+		while (start_stake != 150 || start_stake != 50) {
+			int result = (int) (Math.random() * 10) % 2;
+			if (result == 0) {
+				start_stake -= bet;
+			}
+				else {
+				start_stake += bet;
+		}
+		if (start_stake == 150) {
+			System.out.println("Won the bet:" +start_stake);
+			break;
+		}
+		else if (start_stake == 50) {
+				System.out.println("loose the bet:"+start_stake);
+				break;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
