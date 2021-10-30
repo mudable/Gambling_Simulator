472f82afcdb42cbfc50ce0bfae66bdc86ddaf729
mudable <mudableshilpa@gmail.com>
2021-10-30 14:16:09 +0530

    Added : UC1 to assign given values to variable

---
+++ src/com/bz/gambler/Gambling_Simulator.java
@@ -0,0 +1,10 @@
+package com.bz.gambler;
+
+public class Gambling_Simulator {
+	int start_stake = 100;
+	int bet = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Wel come to Gambling Simulator Solution");
+	}
+}
