a70cb303d934effce2da87d1eb0f23c194db32d2
mudable <mudableshilpa@gmail.com>
2021-11-02 10:45:54 +0530

    Added : UC5 to check the winning or loss of money condition

--- src/com/bz/gambler/Gambling_Simulator.java
+++ src/com/bz/gambler/Gambling_Simulator.java
@@ -3,34 +3,40 @@ package com.bz.gambler;
 public class Gambling_Simulator {
 	/*
 	 * Initialized variable with given values, as start stake and bet. Added random
-	 * method to check win or loose.
+	 * method to check win or loose. Function to calculate the amount of 20 days.
 	 */
-
-	public int makeBet() {
-		int start_stake = 100;
-		int bet = 1;
-
-		while (start_stake != 150 || start_stake != 50) {
+	public int startGambling() {
+		int startingAmount = 100;
+		int bet_Per_Game = 1;
+		while (startingAmount != 150 || startingAmount != 50) {
 			int bet_result = (int) (Math.random() * 10) % 2;
 			if (bet_result == 0) {
-				start_stake -= bet;
+				startingAmount -= bet_Per_Game;
 			} else {
-				start_stake += bet;
+				startingAmount += bet_Per_Game;
 			}
-			if (start_stake == 150) {
+			if (startingAmount == 150) {
 				break;
-			} else if (start_stake == 50) {
+			} else if (startingAmount == 50) {
 				break;
 			}
 		}
-		return start_stake;
+		return startingAmount;
 	}
 
 	public void playFor20Days() {
 		int total_Amount_Invested_In_20_Days = 2000;
 		int amount_After_20Days = 0;
+
 		for (int i = 1; i <= 20; i++) {
-			amount_After_20Days += makeBet();
+			int result = startGambling();
+			amount_After_20Days += result;
+			if (result == 50) {
+				System.out.println("Lost 50$ in day " + i);
+			}
+			if (result == 150) {
+				System.out.println("Won 50$ in day " + i);
+			}
 		}
 		System.out.println("Amount after 20 day is " + amount_After_20Days);
 		if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
