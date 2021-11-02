ac7238c8e4289e620a0bb31377484bbcbf7ca812
mudable <mudableshilpa@gmail.com>
2021-11-02 10:59:18 +0530

    Added:UC7 to play next month

--- src/com/bz/gambler/Gambling_Simulator.java
+++ src/com/bz/gambler/Gambling_Simulator.java
@@ -28,7 +28,7 @@ public class Gambling_Simulator {
 	public void playFor20Days() {
 		int total_Amount_Invested_In_20_Days = 2000;
 		int amount_After_20Days = 0;
-
+		int profit = 0;
 		for (int i = 1; i <= 20; i++) {
 			int result = startGambling();
 			amount_After_20Days += result;
@@ -41,7 +41,7 @@ public class Gambling_Simulator {
 		}
 		System.out.println("Amount after 20 day is " + amount_After_20Days);
 		if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
-			int profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
+			profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
 			System.out.println("Total profit after 20 days is = " + profit);
 
 		} else if (amount_After_20Days < total_Amount_Invested_In_20_Days) {
@@ -49,6 +49,12 @@ public class Gambling_Simulator {
 			System.out.println("Total loss after 20 days is = " + loss);
 		} else
 			System.out.println("No profit No loss");
+		if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
+
+			System.out.println("Won " + profit + "$ Last month. Play Again");
+			playFor20Days();
+		}
+
 	}
 
 	public static void main(String[] args) {
